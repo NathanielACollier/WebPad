3d14bdb61b238a1f74f13fb04f6edefc686c9970
Nathaniel Collier <nathaniel.collier@aecc.com>
2017-12-01 09:18:17 -0600

    dependencies:Testing Folder Browser

---
+++ WebPad.Dependencies.General.Tests/WPFUserControls/LocalFolderBrowser/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using runWPF = WebPad.Dependencies.General.WPFUtilities.Run.Run;
+using System.Threading.Tasks;
+using controls = WebPad.Dependencies.General.WPFUserControls;
+using System.Linq;
+
+namespace WebPad.Dependencies.General.Tests.WPFUserControls.LocalFolderBrowser
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async Task BasicControlDisplayTest()
+        {
+            var result = await runWPF.runWithUIThread(new WPFUtilities.Run.RunOnUIArgs
+            {
+                RunAfterWindowAvailable = (win) =>
+                {
+                    var browser = new controls.LocalFolderBrowser.LocalFolderBrowser();
+                    win.Content = browser;
+                }
+            });
+
+            Assert.IsTrue(result.IsError == false);
+        }
+
+
+        [TestMethod]
+        public void FileExtensionParsingTest()
+        {
+            var browser = new controls.LocalFolderBrowser.LocalFolderBrowser();
+            browser.FileExtensions = "";
+            Assert.IsTrue(browser.FileExtensionsList.Count() == 0 && browser.FileExtensionsList.Any() == false, "List failed to work when empty");
+
+            browser.FileExtensions = "php csv txt;xml; ps1, bat";
+            Assert.IsTrue(browser.FileExtensionsList.Count() == 6, "Failed to split the correct amount of extensions");
+            Assert.IsTrue(string.Equals("ps1", browser.FileExtensionsList.ElementAt(4)), "Failed to identify 4th element as ps1");
+
+            browser.FileExtensions = ".txt, .apple; .fbi.test,xml";
+            Assert.IsTrue(browser.FileExtensionsList.Count() == 4, "Failed to split the correct amount x2");
+            Assert.IsTrue(string.Equals("fbi.test", browser.FileExtensionsList.ElementAt(2)), "Failed to get rid of leading '.' ");
+        }
+
+
+        [TestMethod]
+        public async Task ControlDisplayWithFileExtensionFilter()
+        {
+            var result = await runWPF.runWithUIThread(new WPFUtilities.Run.RunOnUIArgs
+            {
+                RunAfterWindowAvailable = (win) =>
+                {
+                    var browser = new controls.LocalFolderBrowser.LocalFolderBrowser();
+                    win.Content = browser;
+
+                    browser.FileExtensions = "php bat";
+                }
+            });
+
+            Assert.IsTrue(result.IsError == false);
+        }
+    }
+}
---
+++ WebPad.Dependencies.General/WPFUtilities/Run/RunResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebPad.Dependencies.General.WPFUtilities.Run
+{
+    public class RunResult
+    {
+        public bool IsError { get; set; }
+        public Exception ex { get; set; }
+    }
+}

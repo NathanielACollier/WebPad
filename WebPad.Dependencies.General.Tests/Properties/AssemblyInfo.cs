47094890a232d2a42195ab0cdf2532eb6a97b771
Nathaniel A Collier <drxs3v3n@gmail.com>
2021-11-24 12:58:01 -0600

    csproj conversion ran

--- WebPad.Dependencies.General.Tests/Properties/AssemblyInfo.cs
+++ WebPad.Dependencies.General.Tests/Properties/AssemblyInfo.cs
@@ -1,20 +1,7 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("WebPad.Dependencies.General.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebPad.Dependencies.General.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("c8e4cdf5-4e85-4574-9c1a-c11808b225e2")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]

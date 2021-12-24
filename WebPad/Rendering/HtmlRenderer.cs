d2373a55e3c82d7c6378fd823a6a5429310561d0
Nathaniel A Collier <drxs3v3n@gmail.com>
2021-12-24 08:48:54 -0600

    Putting the startup code directly in the document
    This lets us debug
    I can't even get basic javascript code to work in ExecuteScriptAsync, so I don't have something right...

--- WebPad/Rendering/HtmlRenderer.cs
+++ WebPad/Rendering/HtmlRenderer.cs
@@ -77,56 +77,19 @@ namespace WebPad.Rendering
         private async Task<bool> fireOffJavascriptDocumentsetupCode()
         {
             string resultJSONText = await _myBrowser.CoreWebView2.ExecuteScriptAsync(@"
-                frogsound();
-                let hoverGblStyleName = 'webpadGlobalStyles_MouseOver';
-                // create the class we are going to apply on mouse enter
-                let hoverGlbStyle = document.createElement('style');
-                hoverGlbStyle.type = 'text/css';
-                hoverGlbStyle.innerhtml = `
-                    .${hoverGblStyleName} {
-                        background-color: purple;
-                    }
-                `;
-                document.getElementsByTagName('head')[0].appendChild(hoverGblStyle);
-
-                // setup document click to send a message
-                // from: https://stackoverflow.com/questions/29555044/javascript-global-onclick-listener
-                document.addEventListener('click', (e) => {
-                    // element is e.target
-                    // We've got attributes set on every element with what line number and column it is
-                    let lineNumber = e.target.getAttribute('linenumber');
-                    let column = e.target.getAttribute('column');
-
-                    window.chrome.webview.postMessage({
-                        lineNumber: lineNumber,
-                        column: column,
-                        type: 'elementClick'
-                    });
-                });
-
-                // setup the hover so we can highlight the element
-                document.addEventListener('mouseenter',(e)=> {
-                    e.target.classList.add(hoverGblStyleName);
-                });
-
-                document.addEventListener('mouseleave', (e)=> {
-                    e.target.classList.add(hoverGblStyleName);
-                    e.target.classList.remove(hoverGblStyleName);
-                });
-
                 return {
                     success: true
-                }
+                };
             ");
             
             // look at the result
             dynamic result = System.Text.Json.Nodes.JsonNode.Parse(resultJSONText);
-
+/*
             if (result.success == true)
             {
                 return true;
             }
-
+*/
             return false;
         }
 
---
+++ WebPad/Resources/startupCode.js
@@ -0,0 +1,53 @@
+﻿
+docReady(() => {
+   codeToRunOnStartup(); 
+});
+
+
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+function codeToRunOnStartup() {
+    let hoverGblStyleName = 'webpadGlobalStyles_MouseOver';
+    // create the class we are going to apply on mouse enter
+    let hoverGlbStyle = document.createElement('style');
+    hoverGlbStyle.type = 'text/css';
+    hoverGlbStyle.innerhtml = `
+                    .${hoverGblStyleName} {
+                        background-color: purple;
+                    }
+                `;
+    document.getElementsByTagName('head')[0].appendChild(hoverGblStyle);
+
+    // setup document click to send a message
+    // from: https://stackoverflow.com/questions/29555044/javascript-global-onclick-listener
+    document.addEventListener('click', (e) => {
+        // element is e.target
+        // We've got attributes set on every element with what line number and column it is
+        let lineNumber = e.target.getAttribute('linenumber');
+        let column = e.target.getAttribute('column');
+
+        window.chrome.webview.postMessage({
+            lineNumber: lineNumber,
+            column: column,
+            type: 'elementClick'
+        });
+    });
+
+    // setup the hover so we can highlight the element
+    document.addEventListener('mouseenter', (e) => {
+        e.target.classList.add(hoverGblStyleName);
+    });
+
+    document.addEventListener('mouseleave', (e) => {
+        e.target.classList.add(hoverGblStyleName);
+        e.target.classList.remove(hoverGblStyleName);
+    });
+} // end of codeToRunOnStartup

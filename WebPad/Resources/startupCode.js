ebcdb4096a64c22ed75d2b5a592078f814509f5d
Nathaniel A Collier <drxs3v3n@gmail.com>
2021-12-30 18:16:55 -0600

    Hover now works as expected

--- WebPad/Resources/startupCode.js
+++ WebPad/Resources/startupCode.js
@@ -1,34 +1,14 @@
 ﻿
+const hoverGblStyleName = 'webpadGlobalStyles_MouseOver';
+
 docReady(() => {
    codeToRunOnStartup(); 
 });
 
 
-function docReady(fn) {
-    // see if DOM is already available
-    if (document.readyState === "complete" || document.readyState === "interactive") {
-        // call on next available tick
-        setTimeout(fn, 1);
-    } else {
-        document.addEventListener("DOMContentLoaded", fn);
-    }
-}
-
 function codeToRunOnStartup() {
-    let hoverGblStyleName = 'webpadGlobalStyles_MouseOver';
-    // create the class we are going to apply on mouse enter
-    let hoverGlbStyle = document.createElement('style');
-    hoverGlbStyle.type = 'text/css';
-    hoverGlbStyle.appendChild(
-        document.createTextNode(
-        `
-                .${hoverGblStyleName} {
-                    background-color: purple;
-                }
-            `)
-    );
-
-    document.getElementsByTagName('head')[0].appendChild(hoverGlbStyle);
+    
+    setupStyles();
 
     // setup document click to send a message
     // from: https://stackoverflow.com/questions/29555044/javascript-global-onclick-listener
@@ -44,24 +24,61 @@ function codeToRunOnStartup() {
             type: 'elementClick'
         });
     });
-
+    
     // setup the hover so we can highlight the element
     document.addEventListener('mouseover', (e) => {
-        if( e.target && e.target.tagName){
-            console.log(`mouseenter: [tagName: ${e.target.tagName}]`);
-        }
+
         /*
         document doesn't have classList
          */
         if( e.target && e.target != document){
-            e.target.classList.add(hoverGblStyleName);
-            
-            // watch for the leave, and remove the style
-            e.target.addEventListener("mouseleave", (e) => {
-                e.target.classList.remove(hoverGblStyleName);
-            });
+            onNewTargetElement(e.target);
         }
         
     });
     
 } // end of codeToRunOnStartup
+
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+
+function setupStyles(){
+    // create the class we are going to apply on mouse enter
+    let hoverGlbStyle = document.createElement('style');
+    hoverGlbStyle.type = 'text/css';
+    hoverGlbStyle.appendChild(
+        document.createTextNode(
+            `
+                .${hoverGblStyleName} {
+                    background-color: purple;
+                }
+            `)
+    );
+
+    document.getElementsByTagName('head')[0].appendChild(hoverGlbStyle);
+}
+
+let previouseTargetElement = null;
+
+function onNewTargetElement(target) {
+    
+    if( previouseTargetElement ){
+        // remove the class
+        previouseTargetElement.classList.remove(hoverGblStyleName);
+    }
+    
+    if( target && target.tagName){
+        console.log(`onNewTargetElement: [tagName: ${target.tagName}; id: ${target?.id ?? ''}]`);
+        previouseTargetElement = target;
+        
+        target.classList.add(hoverGblStyleName);
+    }
+}

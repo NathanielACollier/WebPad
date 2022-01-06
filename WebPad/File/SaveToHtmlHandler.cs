04f8f248fabe96247d6bbc60039ec4f75f7e2622
Nathaniel Collier <nathaniel.a.collier@gmail.com>
2022-01-06 10:33:17 -0600

    fix html save problem where css didn't save to document

--- WebPad/File/SaveToHtmlHandler.cs
+++ WebPad/File/SaveToHtmlHandler.cs
@@ -17,9 +17,7 @@ public static class SaveToHtmlHandler
                 new System.Text.StringBuilder()
                     .AppendLine("<html>")
                     .AppendLine("<head></head>")
-                    .AppendLine("<body>")
-                    .AppendLine(snippet.Html)
-                    .AppendLine("</body>")
+                    .AppendLine($"<body>{snippet.Html}</body>")
                     .AppendLine("</html>")
                     .ToString()
             );
@@ -31,56 +29,44 @@ public static class SaveToHtmlHandler
             // save CSS
             if (!string.IsNullOrWhiteSpace(snippet.CSS))
             {
-                head.AppendChild(
-                    HtmlAgilityPack.HtmlNode.CreateNode(
-                        new System.Text.StringBuilder()
-                            .AppendLine("<style type='text/css'>")
-                            .AppendLine(snippet.CSS)
-                            .AppendLine("</style>")
-                            .ToString()
-                    )
-                );
+                var s = doc.CreateElement("style");
+                s.SetAttributeValue("type", "text/css");
+                s.AppendChild(doc.CreateTextNode(snippet.CSS));
+                head.AppendChild(s);
             }
             
             // save javascript
             if (!string.IsNullOrWhiteSpace(snippet.Javascript))
             {
-                body.AppendChild(
-                    HtmlAgilityPack.HtmlNode.CreateNode(
-                        new System.Text.StringBuilder()
-                            .AppendLine("<script type='text/javascript'>" )
-                            .AppendLine(snippet.Javascript)
-                            .AppendLine("</script>")
-                            .ToString()
-                    )
-                );
+                var s = doc.CreateElement("script");
+                s.SetAttributeValue("type", "text/javascript");
+                s.AppendChild(doc.CreateTextNode(snippet.Javascript));
+                body.AppendChild(s);
             }
             
-            AppendSnippetReferences(snippet, head);
+            AppendSnippetReferences(snippet,doc, head);
 
             doc.Save(filename: filePath);
         }
 
-        private static void AppendSnippetReferences(SnippetDocumentControl snippet, HtmlNode head)
+        private static void AppendSnippetReferences(SnippetDocumentControl snippet, HtmlAgilityPack.HtmlDocument doc, HtmlNode head)
         {
             // take care of javascript references
             foreach (var reference in snippet.References)
             {
                 if (reference.Type == ReferenceTypes.Css)
                 {
-                    head.AppendChild(
-                        HtmlAgilityPack.HtmlNode.CreateNode(
-                            $"<link rel='stylesheet' href='{reference.Url}'>"
-                            )
-                    );
+                    var s = doc.CreateElement("link");
+                    s.SetAttributeValue("rel", "stylesheet");
+                    s.SetAttributeValue("href", reference.Url);
+                    head.AppendChild(s);
                 }
                 else if (reference.Type == ReferenceTypes.Javascript)
                 {
-                    head.AppendChild(
-                        HtmlAgilityPack.HtmlNode.CreateNode(
-                            $"<script src='{reference.Url}' type='text/javascript'></script>"
-                            )
-                    );
+                    var s = doc.CreateElement("script");
+                    s.SetAttributeValue("src", reference.Url);
+                    s.SetAttributeValue("type", "text/javascript");
+                    head.AppendChild(s);
                 }
                 else
                 {

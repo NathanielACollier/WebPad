a6e76c7b21b032db25aefb0f529cd6e8baac1214
Nathaniel Collier <drxs3v3n@gmail.com>
2022-09-06 16:45:56 -0500

    Setup to deploy the app so it can be distributed with minimal binary files

---
+++ WebPad/deploy-Release.ps1
@@ -0,0 +1,9 @@
+﻿
+$deployPath = "$($env:USERPROFILE)\programs\WebPad"
+$buildConfig = "Release"
+
+& dotnet @("publish","-c", $buildConfig, "-o", "`"$deployPath`"", "-p:PublishSingleFile=true",
+		"-p:RuntimeIdentifier=win-x64", "-p:SelfContained=false", "-p:ExcludeSymbolsFromSingleFile=true",
+		"-nowarn:nu1605"
+ )
+

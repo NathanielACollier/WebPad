cedd29fe895ea2a6b02c46dec2a6f197575954df
Nathaniel Collier <drxs3v3n@gmail.com>
2022-09-07 14:16:03 -0500

    Add emptying folder before publishing

--- WebPad/deploy-Release.ps1
+++ WebPad/deploy-Release.ps1
@@ -2,6 +2,10 @@
 $deployPath = "$($env:USERPROFILE)\programs\WebPad"
 $buildConfig = "Release"
 
+# clear existing folder
+remove-item ([system.io.path]::Combine($deployPath, "*")) -Recurse -Force
+
+
 & dotnet @("publish","-c", $buildConfig, "-o", "`"$deployPath`"", "-p:PublishSingleFile=true",
 		"-p:RuntimeIdentifier=win-x64", "-p:SelfContained=false", "-p:ExcludeSymbolsFromSingleFile=true",
 		"-nowarn:nu1605"
